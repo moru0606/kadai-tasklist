5530cc3e74414fc5780b55bd6f13e36fcd7829da
hiroaki.ooki <moru150606@gmail.com>
2021-07-07 09:55:47 +0000

    tasklist login4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
-     before_action :require_user_logged_in
-     before_action :set_task, only:[:show,:edit,:update,:destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user,only:[:show,:edit]
+    before_action :set_task, only:[:show,:edit,:update,:destroy]
 
     
   def index
@@ -47,6 +48,13 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+          redirect_to root_url
+      end 
+  end
   
   def set_task
       @task = current_user.tasks.find(params[:id])
